61729ab9d5eca24fc35d47161de2eead1f947815
Christopher Wellons <wellons@nullprogram.com>
2015-11-29 09:56:11 -0500

    In rotate_x, use argument rather than uniform.

--- view-frustum-rotation.v.glsl
+++ view-frustum-rotation.v.glsl
@@ -45,8 +45,8 @@ mat4 rotate_x(float theta)
 {
     return mat4(
         vec4(1.0,         0.0,         0.0, 0.0),
-        vec4(0.0,  cos(timer),  sin(timer), 0.0),
-        vec4(0.0, -sin(timer),  cos(timer), 0.0),
+        vec4(0.0,  cos(theta),  sin(theta), 0.0),
+        vec4(0.0, -sin(theta),  cos(theta), 0.0),
         vec4(0.0,         0.0,         0.0, 1.0)
     );
 }
